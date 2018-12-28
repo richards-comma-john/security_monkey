dacf339b6845a2861e84b7adcd650ae6c03fa6ca
richards-comma-john <45633771+richards-comma-john@users.noreply.github.com>
2018-12-27 17:53:23 -0800

    Update watcher.py

--- security_monkey/watcher.py
+++ security_monkey/watcher.py
@@ -642,6 +642,8 @@ class ChangeItem(object):
         Save the item
         """
         app.logger.debug("Saving {}/{}/{}/{}\n\t{}".format(self.index, self.account, self.region, self.name, self.new_config))
+        app.logger.debug("Alerting {}/{}/{}/{}\n\t{}\n\t{}\n\t{}".format(self.index, self.account, self.region, self.name, self.new_config, self.old_config, self.confirmed_new_issues))
+
         self.db_item = datastore.store(
             self.index,
             self.region,
